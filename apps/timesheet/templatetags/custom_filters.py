919d9c0bd133637f6b67f88201f452ceb22f48fe
k1slee <trofimcuk.nikita10@gmail.com>
2026-01-19 14:34:24 +0300

    изменен вывод фио в подписях на тебеле, теперь формат И.И. Иванов

--- apps/timesheet/templatetags/custom_filters.py
+++ apps/timesheet/templatetags/custom_filters.py
@@ -16,3 +16,23 @@ def sum_attr(queryset, attr):
         if value:
             total += value
     return total
+
+@register.filter
+def remove_h(value):
+    """Удаляет букву 'ч' из значения"""
+    if value:
+        return str(value).replace('ч', '').strip()
+    return value
+
+@register.filter
+def format_fio_short(full_name):
+    """Форматирует ФИО в формат И.И. Иванов"""
+    if not full_name:
+        return " "
+    parts = full_name.split()
+
+    if len(parts) == 3:
+        last_name = parts[0]
+        first_initial = parts[1][0] + '.' if parts[1] else ''#Первая буква имени
+        second_initial = parts[2][0] + '.' #Первая буква отчества
+        return f"{first_initial}{second_initial}{last_name}"
